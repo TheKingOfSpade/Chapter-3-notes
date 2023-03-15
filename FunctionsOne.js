15ff335af0f52ca03e74edc2e3e06d946f74e4f9
TheKingOfSpade <122304903+TheKingOfSpade@users.noreply.github.com>
2023-03-15 10:30:15 -0700

    Initial commit

---
+++ FunctionsOne.js
@@ -0,0 +1,39 @@
+//1
+ function sum(num1, num2){
+ console.log(`${num1}*${num2}=${num1*num2}`);
+ } sum(7, 10);
+//2
+ function sum(num1, num2)
+ {    console.log(`${num1}*${num2}=${num1*num2}`);
+ }sum(3.14, 8);
+//3
+function printMax(x, y){
+ if (x>y){ console.log(`the smallest number is ${y}`);
+ } else { console.log(`the larger number is ${x}`);
+   }
+} printMax(16, 9); 
+//4
+  function printMax(x, y){
+   if (x>y){        console.log(`Both the numbers are equal ${x}`);
+   } else {        console.log(`Both the numbers are equal ${y}`);
+   }}printMax(2, 2);
+//5
+  function sum(num1, num2){    console.log(`${num1}*${num2}=${num1*num2}`);
+   sum1(23, 31);
+   function sum1(num3, num4){    console.log(`${num3}*${num4}=${num3*num4}`);
+   }sum1(54, 28);
+   function printMax(){    let sum = 713;
+    let sum1 = 1512;
+    if (sum>sum1){        console.log(`the largest area is ${sum}`);
+    } else {        console.log(`the largest area is ${sum1}`);
+    }}printMax();
+  } 
+ //6
+function sum(num, num2, num3){
+ console.log(`${num} + ${num2} + ${num3} = ${num + num2 + num3}`);
+} sum(450, 300, 500);
+let sum1 = 1250;
+if (sum1 > 1000){
+ console.log(sum1 * 2);
+ console.log(`The charity will receive $2500`)
+} 
