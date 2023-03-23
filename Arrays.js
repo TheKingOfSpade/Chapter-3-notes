03a962a366a715a9da284457e2b8c366905e75ad
TheKingOfSpade <122304903+TheKingOfSpade@users.noreply.github.com>
2023-03-23 10:30:27 -0700

    Initial commit

---
+++ Arrays.js
@@ -0,0 +1,10 @@
+//prints the second value in an array
+function printSecond(arr){
+
+    console.log( arr[1] )
+
+}
+let numbers = ['7', '5', '3', '1'];
+
+let values = [1, 2, 3, 4];
+console.log(numbers()[4-2]);
