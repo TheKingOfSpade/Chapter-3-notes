ce3c44052579cdd851a318e8b1e83e21dea94a9a
TheKingOfSpade <122304903+TheKingOfSpade@users.noreply.github.com>
2023-03-24 10:30:08 -0700

    Initial commit

--- Arrays.js
+++ Arrays.js
@@ -1,10 +1,40 @@
-//prints the second value in an array
-function printSecond(arr){
+//1
+let numbers = [7, 5, 3, 1];
+function printFirst(arr){
 
-    console.log( arr[1] )
+    console.log( arr[0] )
 
 }
-let numbers = ['7', '5', '3', '1'];
+printFirst(numbers)
 
-let values = [1, 2, 3, 4];
-console.log(numbers()[4-2]);
+//2
+let numbers2 = [7, 5, 3, 1];
+function printLast(arr){
+
+    console.log( arr[4] )
+
+}
+printLast(numbers2)
+
+//3
+function emptyArray(arr){
+    if (arr.length ==0){
+        console.log('Empty array')
+    }else{
+        console.log( arr[0] )
+    }
+
+}
+emptyArray( [] )
+
+function emptyArray2(arr){
+    if (arr.length ==0){
+        console.log('Empty array')
+    }else{
+        console.log( arr[3] )
+    }
+
+}
+emptyArray2( [] )
+
+//4
