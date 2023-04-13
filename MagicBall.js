03ad281b175c5068bd72a7beb5a4d98c1a849268
TheKingOfSpade <122304903+TheKingOfSpade@users.noreply.github.com>
2023-04-13 10:29:10 -0700

    Initial commit

---
+++ MagicBall.js
@@ -0,0 +1,15 @@
+const readLine = require(`readline-sync`);
+
+let answer = readLine.question("Welcome to the great fortune place. Do you want to know your fortune? yes or no: ");
+while (answer != "yes" && answer != "no"){
+    console.log("invalid choice");
+    answer = readLine.question("Enter y or n: ");
+}
+
+
+let answerList = ['It is certain.', 'Without a doubt.', 'My reply is no.', 'My sources say no.', 'Ask again later.', 'Reply hazy, try again'];
+
+let randomNum = Math.floor(Math.random()*answerList.length);
+
+let randomAnswer = answerList[randomNum];
+console.log(randomAnswer)
